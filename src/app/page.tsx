ba4553366d1dc43cd33d4f5d1996564681961ae3
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-23 23:48:08 +0900

    💄 childrenのどのページからでも、ヘッダーとサイドバーを気にしなくてよくした (#34)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import { Box, Toolbar } from '@mui/material';
-// import { drawerWidth } from "@/_components/layouts/Sidebar/Sidebar"
-
-const drawerWidth = 320
-
 export default async function Dashboard() {
   return (
-    <main>
-      {/* HederとSidebarのスペース分ずらす*/}
-      <Toolbar />
-      <Box 
-      sx={{
-        boxSizing: 'border-box',
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginRight: `${drawerWidth}px`,
-      }}
-      >
-        <div>
+    <>
+      <div>
         ダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボードダッシュボード
-        </div>
-      </Box>
-    </main>
-  )
+      </div>
+    </>
+  );
 }
