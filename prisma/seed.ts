3d637d56c79f8fee77024f20881504e5db039dcf
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-27 00:56:21 +0900

    🗃️ ユーザーと出費のseedingを行った (#40)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+
 const prisma = new PrismaClient()
 
 const Categories = ["食費", "水道光熱費", "家賃", "娯楽", "衣服・美容", "日用品", "病院代", "交通費", "その他"]
@@ -8,8 +10,10 @@ async function main() {
 
   // カテゴリーの作成
   Categories.forEach(async (value, index) => {
-    const newCategory = await prisma.category.create({
-      data: {
+    const newCategory = await prisma.category.upsert({
+      where: { id: index+1 },
+      update: {},
+      create: {
           id: index+1,
           name: value
       },
@@ -18,6 +22,29 @@ async function main() {
   
   });
 
+  for (let i = 0; i < 3; i++){
+
+    // ユーザーの作成
+    const newUser = await prisma.user.create({
+      data:{
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        passwordDigest: faker.internet.password()
+      },
+    });
+
+    for (let i = 0; i < 20; i++){
+      // 出費の作成
+      const newExpense = await prisma.expense.create({
+        data: {
+          storeName: faker.company.name(),
+          amount: parseInt(faker.finance.amount({ min: 0, max: 30000, dec: 0 })),
+          date: faker.date.between({ from: '2024-08-01T00:00:00.000Z', to: '2024-09-31T00:00:00.000Z' }),
+          userId: newUser.id,
+          categoryId: Math.floor(Math.random() * 9) + 1
+        },
+      });
+    }}
 
 }
 main()
