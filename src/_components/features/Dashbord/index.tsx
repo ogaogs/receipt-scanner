4bea74c932a64b52cbf290ceb9dbfa933daa922e
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-29 04:58:59 +0900

    ♻️ dashbordディレクトリにindexをつけてexportできるようにした (#49)

---
+++ src/_components/features/Dashbord/index.tsx
@@ -0,0 +1,4 @@
+import { UpperDashbord } from "@/_components/features/dashbord/UpperDashbord";
+import { LowerDashbord } from "@/_components/features/dashbord/LowerDashbord";
+
+export { UpperDashbord, LowerDashbord}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
-import { UpperDashbord } from "@/_components/features/dashbord/UpperDashbord";
+import { UpperDashbord, LowerDashbord } from "@/_components/features/dashbord";
 
 export default async function Page() {
   return (
     <>
       <UpperDashbord />
-      <Box sx={{ color: "blue", height: 100 }}>下の段</Box>
+      <LowerDashbord/>
     </>
   );
 }
