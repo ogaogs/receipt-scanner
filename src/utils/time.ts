574804a03ed92e7fa69b18ec3c7a7bb83b3c01d8
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-29 01:38:51 +0900

    ♻️ 関数に返り値の型を追加した (#45)

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,10 +1,15 @@
+type StartAndEndOfMonth = {
+  firstDay: Date
+  lastDay: Date
+}
+
 // 本日の日付を取得(UTC)
-export const getToday = () => {
+export const getToday = ():Date => {
   return new Date()
 } 
 
 // 月の初日と最終日を返す
-export const getStartAndEndOfMonth = (date: Date) => {
+export const getStartAndEndOfMonth = (date: Date):StartAndEndOfMonth => {
   // UTCで表示される
   const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
   const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
