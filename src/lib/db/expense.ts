0aa793ad002c08c14448c9958735e7f5c6abac2a
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-29 17:18:19 +0900

    ⚡️ DBからデータを取得の際にcacheを残すようにした (#49)

--- src/lib/db/expense.ts
+++ src/lib/db/expense.ts
@@ -1,21 +1,20 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { cache } from "react";
 import { Expense } from "@/types";
 
 // 指定した月の出費を取得する
-export const getMonthExpenses = async (
-  userId: string,
-  firstDay: Date,
-  lastDay: Date
-): Promise<Expense[]> => {
-  return await prisma.expense.findMany({
-    where: {
-      date: {
-        gte: firstDay,
-        lt: lastDay,
+export const getMonthExpenses = cache(
+  async (userId: string, firstDay: Date, lastDay: Date): Promise<Expense[]> => {
+    return await prisma.expense.findMany({
+      where: {
+        date: {
+          gte: firstDay,
+          lt: lastDay,
+        },
+        userId: userId,
       },
-      userId: userId,
-    },
-  });
-};
+    });
+  }
+);
