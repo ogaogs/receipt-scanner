69e216009984e298256a556f877623960f74612b
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-30 23:10:00 +0900

    ✨ Siderbarで出費月の範囲の全ての月を取得し"YYYY-MM or M"の形でリストに入れた (#56 #54)

--- src/_components/layouts/Sidebar/Sidebar.tsx
+++ src/_components/layouts/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { getFirstAndLastExpense } from "@/lib/db/index";
+import { getFirstAndLastExpenseDate } from "@/lib/db/index";
 import {
   Box,
   Drawer,
@@ -13,15 +13,50 @@ import {
   Typography,
 } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { FirstAndLastExpenseDate } from "@/types";
 
 const drawerWidth = "20%";
+
+const getMonthsInRange = (
+  firstAndLastExpenseDate: FirstAndLastExpenseDate
+): string[] => {
+  const monthsInRange: string[] = [];
+
+  if (firstAndLastExpenseDate._min.date && firstAndLastExpenseDate._max.date) {
+    // minDateからmaxDateまでの月を取得
+    let currentDate = new Date(
+      firstAndLastExpenseDate._min.date.getFullYear(),
+      firstAndLastExpenseDate._min.date.getMonth(),
+      1
+    );
+    const endDate = new Date(
+      firstAndLastExpenseDate._max.date.getFullYear(),
+      firstAndLastExpenseDate._max.date.getMonth(),
+      1
+    );
+
+    while (currentDate <= endDate) {
+      // 年と月を取得し、"YYYY-MM or M" の形式で格納
+      const year = currentDate.getFullYear();
+      const month = (currentDate.getMonth() + 1).toString();
+      monthsInRange.push(`${year}-${month}`);
+
+      // 次の月に移動
+      currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+  }
+  return monthsInRange;
+};
 
 export const Sidebar = async () => {
   const date = "9月22日";
   const proverb = "時は金なり";
   const userId = "30d06a0b-dcb9-4060-911e-d15b50e2b7e0";
 
-  const firstLastExpense = await getFirstAndLastExpense(userId);
+  const firstLastExpense = await getFirstAndLastExpenseDate(userId);
+
+  const monthsInRange = getMonthsInRange(firstLastExpense);
+  console.log(monthsInRange);
 
   const card = (
     <React.Fragment>
