be8c2b3dd8742971cd8e03ed5b4a311c54d9272e
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-10-26 02:35:48 +0900

    💄 ログインページのUIを整えた (#15)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,61 @@
 import { signIn } from "@/auth";
+import { Button, Card, CardContent, Typography, Box } from "@mui/material";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
   return (
-    <div className="h-screen w-full grid place-content-center">
-      <form
-        action={async () => {
-          "use server";
-          await signIn("google");
+    <Box
+      sx={{
+        paddingTop: "20%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card
+        sx={{
+          width: 300,
+          padding: 3,
+          textAlign: "center",
         }}
       >
-        <button
-          type="submit"
-          className="bg-slate-500 px-6 py-2 rounded-md hover:bg-slate-600 active:scale-95 transition-transform duration-200"
-        >
-          Login with Google
-        </button>
-      </form>
-    </div>
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            家計簿くんへ<br></br>ようこそ
+          </Typography>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          >
+            <Button
+              type="submit"
+              variant="outlined"
+              startIcon={<FcGoogle />}
+              sx={{
+                marginTop: 2,
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                padding: "10px 20px",
+                borderRadius: "4px",
+                backgroundColor: "white",
+                border: "1px solid #ddd",
+                color: "#757575",
+                textTransform: "none",
+                ":hover": {
+                  backgroundColor: "#f7f7f7",
+                  borderColor: "#ccc",
+                },
+              }}
+            >
+              Sign in with Google
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
