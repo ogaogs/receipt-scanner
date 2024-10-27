0a5e10dd53910bc0ce2ba1c39d0c6c572110ae9a
ayumu.ogasawara <ayumu.ogasawara@optim.co.jp>
2024-10-28 02:55:13 +0900

    ✨ signinページを作成した (#123)

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,63 @@
+import { signIn } from "@/auth";
+import { Button, Card, CardContent, Typography, Box } from "@mui/material";
+import { FcGoogle } from "react-icons/fc";
+
+const Page = () => {
+  return (
+    <Box
+      sx={{
+        paddingTop: "10%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        sx={{
+          width: 300,
+          padding: 3,
+          textAlign: "center",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            家計簿くんへ
+            <br />
+            ようこそ
+          </Typography>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          >
+            <Button
+              type="submit"
+              variant="outlined"
+              startIcon={<FcGoogle />}
+              sx={{
+                marginTop: 2,
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                padding: "10px 20px",
+                borderRadius: "4px",
+                backgroundColor: "white",
+                border: "1px solid #ddd",
+                color: "#757575",
+                textTransform: "none",
+                ":hover": {
+                  backgroundColor: "#f7f7f7",
+                  borderColor: "#ccc",
+                },
+              }}
+            >
+              Sign in with Google
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default Page;
